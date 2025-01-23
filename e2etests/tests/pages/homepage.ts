8a3b6bb53b42a23da9ead8cf63c164faf98899e7
Sankar Dakoju <mailtoach79@gmail.com>
2025-01-23 16:47:24 +0000

    Page locator json added and called from page object classes

--- e2etests/tests/pages/homepage.ts
+++ e2etests/tests/pages/homepage.ts
@@ -1,4 +1,5 @@
 import { Page, expect } from "@playwright/test";
+import * as homePageLoc from "../locators/homepageloc.json";
 
 export default class HomePage {
 
@@ -9,12 +10,15 @@ export default class HomePage {
     }
 
     async waitForEditAcInfo() {
-        await this.page.locator("xpath=//a[contains(., 'Edit your account information')]").waitFor({ timeout: 5000 })
-        expect(this.page.locator("xpath=//a[contains(., 'Edit your account information')]")).toBeVisible();
+        // await this.page.locator("xpath=//a[contains(., 'Edit your account information')]").waitFor({ timeout: 5000 })
+        // expect(this.page.locator("xpath=//a[contains(., 'Edit your account information')]")).toBeVisible();
+        await this.page.locator(homePageLoc.EditActLink.locator).waitFor(homePageLoc.EditActLink.actionOptions)
+        expect(this.page.locator(homePageLoc.EditActLink.locator)).toBeVisible();
     }
 
     async homePageDisplayed() {
-        await this.page.locator("xpath=//*[@id='column-right']/div/a[14]").click();
+        // await this.page.locator("xpath=//*[@id='column-right']/div/a[14]").click();
+        await this.page.locator(homePageLoc.logOutLink.locator).click();
         await this.page.getByRole("link", { name: 'Continue' }).click();
     }
 
--- e2etests/tests/pages/loginpage.ts
+++ e2etests/tests/pages/loginpage.ts
@@ -1,4 +1,5 @@
 import { Page } from "@playwright/test";
+import * as loginPageLoc from "../locators/loginpageloc.json";
 
 export default class LoginPage {
 
@@ -10,12 +11,16 @@ export default class LoginPage {
 
     async goToLoginPage() {
         await this.page.goto("https://ecommerce-playground.lambdatest.io/");
-        await this.page.locator("xpath=//a[contains(., 'My account') and @data-toggle]").click();
+        // await this.page.locator("xpath=//a[contains(., 'My account') and @data-toggle]").click();
+        await this.page.locator(loginPageLoc.loginLink.locator).click();
     }
 
     async loginToApp() {
-        await this.page.getByPlaceholder("E-Mail Address").fill('dummy1234@gmail.com');
-        await this.page.getByPlaceholder("Password").fill('dummy1234@gmail.com');
-        await this.page.locator("xpath=//input[@value='Login']").click();
+        // await this.page.getByPlaceholder("E-Mail Address").fill('dummy1234@gmail.com');
+        // await this.page.getByPlaceholder("Password").fill('dummy1234@gmail.com');
+        // await this.page.locator("xpath=//input[@value='Login']").click();
+        await this.page.locator(loginPageLoc.emailField.locator).fill('dummy1234@gmail.com');
+        await this.page.locator(loginPageLoc.passwordField.locator).fill('dummy1234@gmail.com');
+        await this.page.locator(loginPageLoc.loginBtn.locator).click();
     }
 }
